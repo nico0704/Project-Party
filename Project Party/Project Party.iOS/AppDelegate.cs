3345a170b715d03af97b0d26f0a55c58f235e50e
Patryk <stemper0123@gmail.com>
2022-08-11 21:36:44 +0200

    11.08 Map init

--- Project Party/Project Party.iOS/AppDelegate.cs
+++ Project Party/Project Party.iOS/AppDelegate.cs
@@ -22,7 +22,9 @@ namespace Project_Party.iOS
         //
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
+            
             global::Xamarin.Forms.Forms.Init();
+            Xamarin.FormsMaps.Init();
             LoadApplication(new App());
 
             return base.FinishedLaunching(app, options);
---
+++ Project Party/Project Party/Views/MapPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Project_Party.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+using Xamarin.Forms.Xaml;
+
+namespace Project_Party.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MapPage : ContentPage
+    {
+        public MapPage()
+        {
+            InitializeComponent();
+            this.BindingContext = new MapPageViewModel();
+            
+        }
+    }
+}
